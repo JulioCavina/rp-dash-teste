286281961a425a5891d56916c288c8e9cf36f63c
JulioCavina <74439658+JulioCavina@users.noreply.github.com>
2025-12-12 17:54:38 -0300

    Hide Streamlit main menu and footer
    Added custom CSS to hide Streamlit's main menu and footer.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 # streamlit_app.py
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
 import os
 import streamlit as st
 from PIL import Image
