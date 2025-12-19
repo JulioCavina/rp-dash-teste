9f685b33ce8e1895b699de81ec3e48fcc54b2119
JulioCavina <74439658+JulioCavina@users.noreply.github.com>
2025-12-19 16:26:14 -0300

    Delete crowley directory

--- crowley/flight.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render(df_crowley, cookies, data_atualizacao):
-    if st.button("Voltar ao Menu Crowley"):
-        st.query_params["view"] = "menu"
-        st.rerun()
-    st.subheader("Relatório Flight")
-    st.info("Módulo em desenvolvimento.")
--- crowley/ranking_analitico.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render(df_crowley, cookies, data_atualizacao):
-    if st.button("Voltar ao Menu Crowley"):
-        st.query_params["view"] = "menu"
-        st.rerun()
-    st.subheader("Ranking Analítico")
-    st.info("Módulo em desenvolvimento.")
